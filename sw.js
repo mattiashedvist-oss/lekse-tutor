661fdae22dbf3352058328ce3e5476d7acf4059c
Mattias Hedqvist <mattias.hedqvist@gmail.com>
2026-04-10 21:39:19 +0200

    Ta bort rating-rad fra tutor-svar for mer plass på mobil

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lekse-tutor-v8';
+const CACHE = 'lekse-tutor-v9';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {

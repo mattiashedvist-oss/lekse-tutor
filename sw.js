1cf7f62ed2cd26fc49d4e7824ee718a827fd9fdf
Mattias Hedqvist <mattias.hedqvist@gmail.com>
2026-04-10 21:02:54 +0200

    Fix all mobile overflow - contain content within screen width

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lekse-tutor-v6';
+const CACHE = 'lekse-tutor-v7';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {

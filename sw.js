fd1bfbb1a864102df3e6b6a836061a738b768484
Mattias Hedqvist <mattias.hedqvist@gmail.com>
2026-04-10 21:58:27 +0200

    Chat-at-bottom layout, lärarpersona med teach/quick toggle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lekse-tutor-v10';
+const CACHE = 'lekse-tutor-v11';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.svg', '/icon-512.svg'];
 
 self.addEventListener('install', e => {
